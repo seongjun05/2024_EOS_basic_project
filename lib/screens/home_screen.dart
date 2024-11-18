649a7be8b6dda7fe6afd343cbfea5c8b7ee262f1
seongjun <tjdwns135780@naver.com>
2024-11-18 18:56:47 +0900

    asdf

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:eos_todolist1/screens/setting_screen.dart';
 import 'package:eos_todolist1/widgets/add_button.dart';
 import 'package:eos_todolist1/widgets/todo_item.dart';
 import 'package:flutter/material.dart';
@@ -29,8 +30,11 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(
       backgroundColor: Color(0xFFA4C639).withOpacity(0.1),
-      title: Text('Eos ToDoList'),
+      title: Text('EOS ToDoList'),
       leading: Image.asset('assets/images/eos_logo.png.png'),
+      actions: [IconButton(onPressed: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => SettingScreen()));
+      }, icon: Icon(Icons.settings_rounded))],
     ),
         body: Column(
           children: [
@@ -42,14 +46,15 @@ class _HomeScreenState extends State<HomeScreen> {
                   Container(
                       width: 140,
                       height: 140,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(width: 10, color: Colors.grey),
-                          borderRadius: BorderRadius.circular(200)
-                      ),
-                      child: Center(
-                          child: Image.asset('assets/images/eos_logo.png.png')
-                      )
+                      // decoration: BoxDecoration(
+                      //     color: Colors.white,
+                      //     border: Border.all(width: 10, color: Colors.grey),
+                      //     borderRadius: BorderRadius.circular(200)
+                      // ),
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(80),
+                        child: Image.asset('assets/images/img.png', fit: BoxFit.cover,),
+              )
                   ),
                   SizedBox(
                     width: 35,
@@ -151,7 +156,7 @@ class _HomeScreenState extends State<HomeScreen> {
           Navigator.of(context).pop(); },
           child: Text('취소', style: TextStyle(color: Colors.black),),
          ),
-        TextButton(onPressed: () {
+          TextButton(onPressed: () {
           if(_textController.text.isNotEmpty){
             setState(() {
               toDoList.add(_textController.text);
